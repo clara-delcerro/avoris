f368de3c961340526005fdeabcdc83061b9f5987
Clara <claradelcerrolopez@gmail.com>
2025-11-24 13:58:56 +0100

    hero started swiper slider

--- src/app/home/hero/hero.component.ts
+++ src/app/home/hero/hero.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+Swiper.use([Navigation, Pagination]);
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+  styleUrl: './hero.component.scss',
 })
-export class HeroComponent {
 
+
+export class HeroComponent implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    new Swiper('.hero__slider', {
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+      },
+      navigation: {
+        nextEl: '.hero__arrow--next',
+        prevEl: '.hero__arrow--prev'
+      }
+    });
+  }
 }
