900c14996e0ca59c90b5ef62dedc30349970359f
Clara <claradelcerrolopez@gmail.com>
2025-11-24 18:43:04 +0100

    added footer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
-import { FiltersSidebarComponent } from './filters-sidebar/filters-sidebar.component';
-import { HeroComponent } from './hero/hero.component';
-import { PriceModalComponent } from './price-modal/price-modal.component';
+import { FiltersSidebarComponent } from './components/filters-sidebar/filters-sidebar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { PriceModalComponent } from './components/price-modal/price-modal.component';
 import { HeaderComponent } from '../shared/header/header.component';
 import { FooterComponent } from '../shared/footer/footer.component';
 import { CardsGridComponent } from '../shared/cards-grid/cards-grid.component';
