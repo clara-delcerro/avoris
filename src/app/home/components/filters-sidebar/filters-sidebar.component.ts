546ab0dfbd4ad1c348b453b23b743ca79ccb57ad
clara <clara.delcerro@inetum.com>
2025-11-25 22:37:47 +0100

    Started filters-sidebar

--- src/app/home/components/filters-sidebar/filters-sidebar.component.ts
+++ src/app/home/components/filters-sidebar/filters-sidebar.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-filters-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './filters-sidebar.component.html',
-  styleUrl: './filters-sidebar.component.scss'
+  styleUrls: ['./filters-sidebar.component.scss']
 })
 export class FiltersSidebarComponent {
+
+  isOpen = false;
+
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
+
+  close() {
+    this.isOpen = false;
+  }
+
+  open() {
+    this.isOpen = true;
+  }
+
+  toggleSection(section: string) {
+    this.openSections[section] = !this.openSections[section];
+  }
+
+  openSections: Record<string, boolean> = {
+    destinos: true,
+    aventura: true,
+    alojamiento: true,
+    precio: true
+  };
 
 }
